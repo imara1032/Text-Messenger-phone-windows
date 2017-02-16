c593fdd8445560151e0aa76b6c111b0ee65d1268
imara1032 <idharmakirthimudiyanselage@csumb.edu>
2017-02-16 12:46:18 -0800

    setting up sqlLight db

--- TextMessenger/TextMessenger/FirmRegister.Designer.cs
+++ TextMessenger/TextMessenger/FirmRegister.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TextMessenger
 {
-    partial class Form1
+    partial class FirmRegister
     {
         /// <summary>
         /// Required designer variable.
@@ -28,11 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.confirm = new System.Windows.Forms.Button();
+            this.grbRequestCode = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.phoneNumber = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.request = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.code = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grbRequestCode.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // confirm
+            // 
+            this.confirm.Location = new System.Drawing.Point(234, 71);
+            this.confirm.Name = "confirm";
+            this.confirm.Size = new System.Drawing.Size(75, 23);
+            this.confirm.TabIndex = 0;
+            this.confirm.Text = "Confirm";
+            this.confirm.UseVisualStyleBackColor = true;
+            // 
+            // grbRequestCode
+            // 
+            this.grbRequestCode.Controls.Add(this.request);
+            this.grbRequestCode.Controls.Add(this.label2);
+            this.grbRequestCode.Controls.Add(this.name);
+            this.grbRequestCode.Controls.Add(this.phoneNumber);
+            this.grbRequestCode.Controls.Add(this.label1);
+            this.grbRequestCode.Location = new System.Drawing.Point(12, 12);
+            this.grbRequestCode.Name = "grbRequestCode";
+            this.grbRequestCode.Size = new System.Drawing.Size(315, 114);
+            this.grbRequestCode.TabIndex = 1;
+            this.grbRequestCode.TabStop = false;
+            this.grbRequestCode.Text = "Step 1: Request Code";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phone Number: ";
+            // 
+            // phoneNumber
+            // 
+            this.phoneNumber.Location = new System.Drawing.Point(90, 19);
+            this.phoneNumber.Name = "phoneNumber";
+            this.phoneNumber.Size = new System.Drawing.Size(139, 20);
+            this.phoneNumber.TabIndex = 1;
+            this.phoneNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(90, 55);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(139, 20);
+            this.name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Full Name:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // request
+            // 
+            this.request.Location = new System.Drawing.Point(234, 81);
+            this.request.Name = "request";
+            this.request.Size = new System.Drawing.Size(75, 23);
+            this.request.TabIndex = 4;
+            this.request.Text = "Request";
+            this.request.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.code);
+            this.groupBox1.Controls.Add(this.confirm);
+            this.groupBox1.Location = new System.Drawing.Point(12, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Step 2: Confirm Code";
+            // 
+            // code
+            // 
+            this.code.Location = new System.Drawing.Point(90, 31);
+            this.code.Name = "code";
+            this.code.Size = new System.Drawing.Size(139, 20);
+            this.code.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SMS Code:";
+            // 
+            // FirmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(339, 258);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grbRequestCode);
+            this.Name = "FirmRegister";
+            this.Text = "Register";
+            this.grbRequestCode.ResumeLayout(false);
+            this.grbRequestCode.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button confirm;
+        private System.Windows.Forms.GroupBox grbRequestCode;
+        private System.Windows.Forms.TextBox phoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button request;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox code;
     }
 }
--- TextMessenger/TextMessenger/FirmRegister.cs
+++ TextMessenger/TextMessenger/FirmRegister.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace TextMessenger
 {
-    public partial class Form1 : Form
+    public partial class FirmRegister : Form
     {
-        public Form1()
+        public FirmRegister()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
